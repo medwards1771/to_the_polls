a7d4b8036a4af8abe881f8c494b8c6ccc0c67c01
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-09-18 14:47:56 -0400

    Actually complete part 3 of tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
-
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
